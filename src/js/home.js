4128b49409e0fe1746c178bf76b44c2b059e0a1d
姚军 <yaojun@chinafintech.cn>
2017-09-08 18:10:35 +0800

    添加swiper

--- src/js/home.js
+++ src/js/home.js
@@ -1,24 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-
-
+import Swipe from "vue-awesome-swiper";
+import "../less/home.less";
 
 const number=require("./common");
-import "./style.less";
-Vue.config.productionTip = false
+require('swiper/dist/css/swiper.css');
+const swiper=Swipe.swiper;
+const swiperSlide=Swipe.swiperSlide;
+Vue.use(Swipe);
+Vue.config.productionTip=false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#home-app',
-  data(){
-    return {
-      message:"home page  -- dsx"+ number
+    components: {swiper, swiperSlide},
+    el: '#home-app',
+    data() {
+        return {
+            message: "home page  -- dsx"+number,
+            slides:[1,2,3,4]
+        }
+    },
+
+     methods: {
+        changeMessage() {
+            this.message="new ..message";
+        }, addSlide() {
+            this.slides.push(this.slides[this.slides.length-1]+1);
+        }
     }
-  }
-  ,methods:{
-    changeMessage(){
-      this.message="new ..message";
-    }
-  }
 })
